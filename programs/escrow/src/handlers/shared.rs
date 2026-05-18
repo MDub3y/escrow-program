a797bbb35ca5ef71e4f2b09681886e0f17d56b67
Awesxme <manasvi.d007@gmail.com>
2026-05-18 18:18:34 +0000

    added shared for handlers

---
+++ programs/escrow/src/handlers/shared.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::token_interface::{
+    transfer_checked, TokenAccount, TokenInterface, TransferChecked,
+};
+
+pub fn transfer_tokens<'info>(
+    from: &InterfaceAccount<'info, TokenAccount>,
+    to: &InterfaceAccount<'info, TokenAccount>,
+    amount: &u64,
+    mint: &InterfaceAccount<'info, Mint>,
+    authority: &AccountInfo<'info>,
+    token_program: &Interface<'info, TokenInterface>,
+    owning_pda_seeds: Option<&[&[u8]]>,
+) -> Result<()> {
+    let transfer_accounts = TransferChecked {
+        from: from.to_account_info(),
+        mint: mint.to_account_info(),
+        to: to.to_account_info(),
+        authority: authority.to_account_info(),
+    };
+
+    let signers_seeds = owning_pda_seeds.map(|seeds| [seeds]);
+
+    transfer_checked(
+        if let Some(seeds_arr) = signers_seeds.as_ref() {
+            CpiContext::new_with_signer(
+                token_program.to_account_info(),
+                transfer_accounts,
+                seeds_arr,
+            )
+        } else {
+            CpiContext::new(token_program.to_account_info(), transfer_accounts)
+        },
+        *amount,
+        mint.decimals,
+    )
+}
