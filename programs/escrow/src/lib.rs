d6c6df715316f40b5c1becb1d3aa9e7f826a8ed9
Awesxme <manasvi.d007@gmail.com>
2026-05-19 16:23:50 +0000

    added instructions in lib

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -10,11 +10,16 @@ declare_id!("B2fbWZXvAZGdjBV7bwt5LzsD5VPwwf6errc134sVHzdr");
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amt: u64,
+        token_b_wanted_amt: u64,
+    ) -> Result<()> {
+        handlers::make_offer::make_offer(context, id, token_a_offered_amt, token_b_wanted_amt)
+    }
+
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        handlers::take_offer::take_offer(context)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
